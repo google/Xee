Fixed off-by-one error: Xee bounds have parity with `computePixels()`
In addition to this fix, we add the beginning for support of passing in an `ee.Geometry()` into `xr.open_dataset()`.

PiperOrigin-RevId: 563532251

xee/ext.py
@@ -21,6 +21,7 @@ import functools
 import math
 import os
 from typing import Any, Iterable, Literal, Optional, Union
+import warnings
 
 import numpy as np
 import pyproj
@@ -86,13 +87,6 @@ class EarthEngineStore(common.AbstractDataStore):
       n_images: int = -1,
       **ee_kwargs: ...,
   ):
-    self.chunks = self.PREFERRED_CHUNKS.copy()
-    if chunks == -1:
-      self.chunks = -1
-    # TODO(b/291851322): Consider support for laziness when chunks=None.
-    elif chunks is not None and chunks != 'auto':
-      self.chunks.update(chunks)
-
     self.image_collection = image_collection
     if n_images != -1:
       self.image_collection = image_collection.limit(n_images)
@@ -119,10 +113,23 @@ class EarthEngineStore(common.AbstractDataStore):
     # Scale in the projection's units. Typically, either meters or degrees.
     # If we use the default CRS i.e. EPSG:3857, the units is in meters.
     default_scale = self.SCALE_UNITS.get(self.scale_units, 1)
+    # TODO(alxr): Calculate the default scale based on the transformation val
+    # from ee
     self.scale = ee_kwargs.get('scale', default_scale)
-    x_min_0, y_min_0, x_max_0, y_max_0 = self.crs.area_of_use.bounds
-    x_min, y_min = self.project(x_min_0, y_min_0)
-    x_max, y_max = self.project(x_max_0, y_max_0)
+    try:
+      x_min_0, y_min_0, x_max_0, y_max_0 = self.crs.area_of_use.bounds
+    except AttributeError:
+      x_min_0, y_min_0, x_max_0, y_max_0 = _geom_to_bounds(
+          self.image_collection.first().geometry()
+      )
+    # We add and subtract the scale to solve an off-by-one error. With this
+    # adjustment, we achieve parity with a pure `computePixels()` call.
+    x_min, y_min = self.project(x_min_0 - self.scale, y_min_0)
+    if _bounds_are_invalid(x_min, y_min):
+      x_min, y_min = self.project(x_min_0, y_min_0)
+    x_max, y_max = self.project(x_max_0, y_max_0 + self.scale)
+    if _bounds_are_invalid(x_max, y_max):
+      x_max, y_max = self.project(x_max_0, y_max_0)
     self.bounds = x_min, y_min, x_max, y_max
     # Get the dimensions name based on the CRS (scale units).
     self.dimension_names = self.DIMENSION_NAMES.get(
@@ -136,6 +143,50 @@ class EarthEngineStore(common.AbstractDataStore):
 
     self.ee_kwargs = ee_kwargs
     self._props.update(crs=self.crs_arg)
+
+    self.chunks = self.PREFERRED_CHUNKS.copy()
+    if chunks == -1:
+      self.chunks = -1
+    # TODO(b/291851322): Consider support for laziness when chunks=None.
+    elif chunks is not None and chunks != 'auto':
+      self.chunks = self._assign_index_chunks(chunks)
+
+    self.preferred_chunks = self._assign_preferred_chunks()
+
+  def _assign_index_chunks(
+      self, input_chunk_store: dict[Any, Any]
+  ) -> dict[Any, Any]:
+    """Assigns values of 'index', 'width', and 'height' to `self.chunks`.
+
+    This method first attempts to retrieve values for 'index', 'width',
+    and 'height' from the 'input_chunk_store' dictionary. If the values are not
+    found in 'input_chunk_store', it falls back to default values stored in
+    'self.PREFERRED_CHUNKS'.
+    Args:
+      input_chunk_store (dict): how to break up the data into chunks.
+    Returns:
+      dict: A dictionary containing 'index', 'width', and 'height' values.
+    """
+    chunks = {}
+    y_dim_name, x_dim_name = self.dimension_names
+    chunks['index'] = (input_chunk_store.get(self.primary_dim_name)
+                       or input_chunk_store.get('index')
+                       or self.PREFERRED_CHUNKS['index'])
+    chunks['width'] = (input_chunk_store.get(y_dim_name)
+                       or input_chunk_store.get('width')
+                       or self.PREFERRED_CHUNKS['width'])
+    chunks['height'] = (input_chunk_store.get(x_dim_name)
+                        or input_chunk_store.get('height')
+                        or self.PREFERRED_CHUNKS['height'])
+    return chunks
+
+  def _assign_preferred_chunks(self) -> Chunks:
+    chunks = {}
+    y_dim_name, x_dim_name = self.dimension_names
+    chunks[self.primary_dim_name] = self.chunks['index']
+    chunks[y_dim_name] = self.chunks['width']
+    chunks[x_dim_name] = self.chunks['height']
+    return chunks
 
   def project(self, xs: float, ys: float) -> tuple[float, float]:
     transformer = pyproj.Transformer.from_crs(
@@ -166,7 +217,7 @@ class EarthEngineStore(common.AbstractDataStore):
         'scale_factor': arr.scale,
         'scale_units': self.scale_units,
         'dtype': data.dtype,
-        'preferred_chunks': self.PREFERRED_CHUNKS,
+        'preferred_chunks': self.preferred_chunks,
         'bounds': arr.bounds,
     }
 
@@ -204,11 +255,12 @@ class EarthEngineStore(common.AbstractDataStore):
     try:
       primary_coord = self._get_primary_dim_values()
     except (ee.EEException, ValueError) as e:
-      print(
-          f'Error while fetching {self.primary_dim_property!r} values from an '
+      warnings.warn(
+          f'Could not fetch {self.primary_dim_property!r} values from an '
           f'ImageCollection due to {e}.'
       )
       primary_coord = np.arange(v0.shape[0])
+
     coords = [
         (
             self.primary_dim_name,
@@ -223,6 +275,10 @@ class EarthEngineStore(common.AbstractDataStore):
   def close(self) -> None:
     # TODO(alxr): Do I want to do this?
     del self.image_collection
+
+
+def _bounds_are_invalid(x: float, y: float) -> bool:
+  return math.isnan(x) or math.isnan(y) or math.isinf(x) or math.isinf(y)
 
 
 def _parse_dtype(data_type: types.DataType):
@@ -251,6 +307,19 @@ def _parse_dtype(data_type: types.DataType):
     dt = getattr(np, type_)
 
   return np.dtype(dt)
+
+
+def _geom_to_bounds(geom: ee.Geometry) -> tuple[float, float, float, float]:
+  """Finds the bounding box from a ee.Geometry polygon."""
+  bounds = geom.bounds().getInfo()
+  coords = np.array(bounds['coordinates'], dtype=np.float32)[0]
+  x_min, y_min, x_max, y_max = (
+      min(coords[:, 0]),
+      min(coords[:, 1]),
+      max(coords[:, 0]),
+      max(coords[:, 1]),
+  )
+  return x_min, y_min, x_max, y_max
 
 
 class _GetComputedPixels: