e7c99adf941437a281ccfbd954abf53bc7cc91be
dabhicusp <sahildabhi@infocusp.com>
2023-10-31 17:08:22 +0000

    Converting non-nanosecond precision to nanosecond.

--- xee/ext.py
+++ xee/ext.py
@@ -30,6 +30,7 @@ import warnings
 
 import affine
 import numpy as np
+import pandas as pd
 import pyproj
 from pyproj.crs import CRS
 import xarray
@@ -527,7 +528,7 @@ class EarthEngineStore(common.AbstractDataStore):
       )
     if self.primary_dim_property in ['system:time_start', 'system:time_end']:
       # Convert elements in primary_dim_list to np.datetime64
-      primary_coords = [np.datetime64(time, 'ms') for time in primary_coords]
+      primary_coords = [pd.to_datetime(time, unit='ms') for time in primary_coords]
     return primary_coords
 
   def get_variables(self) -> utils.Frozen[str, xarray.Variable]:
