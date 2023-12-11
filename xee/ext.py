00ba44476bb5141a15d54f074f7f565bce677a01
Rylan Boothman <boothmanrylan@gmail.com>
2023-12-11 13:51:32 -0500

    use pixelCoordinates instead of pixelLonLat

--- xee/ext.py
+++ xee/ext.py
@@ -253,10 +253,6 @@ class EarthEngineStore(common.AbstractDataStore):
     else:
       self.mask_value = mask_value
 
-    self.coordinate_transformer = pyproj.Transformer.from_crs(
-        'EPSG:4326', self.crs, always_xy=True
-    )
-
   @functools.cached_property
   def get_info(self) -> Dict[str, Any]:
     """Make all getInfo() calls to EE at once."""
@@ -555,23 +551,15 @@ class EarthEngineStore(common.AbstractDataStore):
           f'ImageCollection due to: {e}.'
       )
 
-    lnglat_img = ee.Image.pixelLonLat()
+    lnglat_img = ee.Image.pixelCoordinates(ee.Projection(self.crs_arg))
     lon_grid = self.project((0, 0, v0.shape[1], 1))
     lat_grid = self.project((0, 0, 1, v0.shape[2]))
     lon = self.image_to_array(
-        lnglat_img,
-        grid=lon_grid,
-        dtype=np.float32,
-        bandIds=['longitude', 'latitude'],
+        lnglat_img, grid=lon_grid, dtype=np.float32, bandIds=['x']
     )
-    lon = self.coordinate_transformer.transform(lon[0], lon[1])[0]
     lat = self.image_to_array(
-        lnglat_img,
-        grid=lat_grid,
-        dtype=np.float32,
-        bandIds=['longitude', 'latitude'],
+        lnglat_img, grid=lat_grid, dtype=np.float32, bandIds=['y']
     )
-    lat = self.coordinate_transformer.transform(lat[0], lat[1])[1]
     width_coord = np.squeeze(lon)
     height_coord = np.squeeze(lat)
 
