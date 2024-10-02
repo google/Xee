6c5a416a4067036363d7dbf966da4411609d12d9
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-10-02 11:27:22 +0000

    refactor: modern sphinx implementation

--- docs/conf.py
+++ docs/conf.py
@@ -1,83 +1,44 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
 
-# -- Path setup --------------------------------------------------------------
+This file only contains a selection of the most common options. For a full
+list see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-# Print Python environment info for easier debugging on ReadTheDocs
-
+# -- Path setup ----------------------------------------------------------------
 import subprocess
 import sys
-import xee  # verify this works
+import xee
 
-print('python exec:', sys.executable)
-print('sys.path:', sys.path)
-print('pip environment:')
-subprocess.run([sys.executable, '-m', 'pip', 'list'])  # pylint: disable=subprocess-run-check
+# -- Project information -------------------------------------------------------
+project = "Xee"
+copyright = "2023, Google LCC"
+author = "The Xee authors"
+# wait for https://github.com/google/Xee/pull/162
+# release = xee.__version__
 
-print(f'xee: {xee.__file__}')
-
-# -- Project information -----------------------------------------------------
-
-project = 'Xee'
-copyright = '2023, Google LCC'  # pylint: disable=redefined-builtin
-author = 'The Xee authors'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# -- General configuration -----------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
     "myst_parser",
 ]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "_templates", "Thumbs.db", ".DS_Store"]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '_templates', 'Thumbs.db', '.DS_Store']
-
+# -- Options for intersphinx output --------------------------------------------
 intersphinx_mapping = {
-    'xarray': ('https://xarray.pydata.org/en/latest/', None),
+    "xarray": ("https://xarray.pydata.org/en/latest/", None),
 }
 
 # -- Options for HTML output -------------------------------------------------
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_logo = "_static/xee-logo.png"
+html_favicon = "_static/xee-logo.png"
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# -- Extension config
-
+# -- Options for autosummary/autodoc output ------------------------------------
 autosummary_generate = True
-
-# https://myst-nb.readthedocs.io/en/latest/use/execute.html
-jupyter_execute_notebooks = 'cache'
-# https://myst-nb.readthedocs.io/en/latest/use/formatting_outputs.html#removing-stdout-and-stderr
-nb_output_stderr = 'remove-warn'
-
-# https://stackoverflow.com/a/66295922/809705
-autodoc_typehints = 'description'
+autodoc_typehints = "description"
